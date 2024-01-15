2c0392aa4067c0aec7d4346bda57befc4db87001
antonsotnik <as@trueflip.io>
2024-01-15 12:38:00 +0300

    create tests

--- meadowlark.js
+++ meadowlark.js
@@ -1,39 +1,28 @@
 const express = require('express')
 // const expressHandlebars = require('express-handlebars')
-const { engine } = require('express-handlebars')
+const {engine} = require('express-handlebars')
 const app = express()
 const port = process.env.PORT || 3040
-const fortune = require('./lib/fortune')
+const handlers = require('./lib/handler')
 
+// Статичные данные
 app.use(express.static(__dirname + `/public`))
 
+// Использование шаблонизатора handlebars
 app.engine('hbs', engine({
     extname: 'hbs',
     defaultLayout: 'main',
     layoutsDir: 'views/layouts'
 }))
-
 app.set('view engine', '.hbs')
+// Пользовательские страницы
+app.get('/', handlers.home)
+app.get('/about', handlers.about)
 
-app.get('/', (req, res) => res.render('home'))
-
-app.get('/about', (req, res) => {
-    res.render('about', { fortune: fortune.getFortune() })
-})
-
-// Пользовательская страница 404
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
-
-// Пользовательская страница 500
-app.use((err, req, res) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+// Пользовательская страница 404/500
+app.use(handlers.notFound)
+app.use(handlers.serverError)
 
 app.listen(port, () => console.log(
-    `Express запущен на http://localhost:${port}; нажмите Ctrl+C для завершения.` )
+    `Express запущен на http://localhost:${port}; нажмите Ctrl+C для завершения.`)
 )
